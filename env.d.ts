60df50d8a5e5dcd5b1afc634c8e83efa1fb8d0ba
Dmytro <dmitry.yurin2020@gmail.com>
2024-01-19 15:30:58 +0200

    custom server & payload cms setup

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    PAYLOAD_SECRET: string;
+    MONGODB_URL: string;
+  }
+}
---
+++ src/next-utils.ts
@@ -0,0 +1,10 @@
+import next from "next";
+
+const port = Number(process.env.PORT) || 3000;
+
+export const nextApp = next({
+  dev: process.env.NODE_ENV !== "production",
+  port,
+});
+
+export const nextHandler = nextApp.getRequestHandler();
