119efc4cf6f23b03668bc5781ba8c52143817fb3
Dmytro <dmitry.yurin2020@gmail.com>
2024-01-19 17:09:49 +0200

    setup trpc with react-query and express adapter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar/Navbar";
+import Providers from "@/components/Providers";
+import { Inter } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +25,10 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">{children}</div>
+          </Providers>
         </main>
       </body>
     </html>
